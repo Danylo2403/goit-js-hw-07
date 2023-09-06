155c0f60a77c491f2adf824994fb7eb5a2c7f0eb
Danylo2403 <daniilkon24@gmail.com>
2023-09-06 18:06:47 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,27 +1,12 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-const gallery = document.querySelector(".gallery");
-//*
-const galleryLi = galleryItems
-    .map(({ preview, original, description }) =>
-        `<li data-preview="${preview}" class="gallery__item js-product-item">
-        <a class="gallery__link" href="${original}">
-        <img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"/>
-        </a>
-        </li>`
-    ).join('');
+const gallery = document.querySelector('.gallery');
 
-gallery.innerHTML = galleryLi;
+const markup = createMarkup(galleryItems)
 
-// console.log(galleryItems);
-
-gallery.addEventListener(`click`, evt => {
-    evt.preventDefault();
-    if (evt.target.targetName === `IMG`) {
-        const largeImageSrc = evt.target.getAttribute('data-source');
-        const lightbox = basicLightbox.create(`
-        <img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"/>`)
-        lightbox.show();
-    }
-});
+function createMarkup(arr) {
+  return arr.map((card) => {
+    console.log(card);
+    return card; // Повертаємо оригінальний об'єкт card
+  });
+}
