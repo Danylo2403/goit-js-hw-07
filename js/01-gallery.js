b0daaacaa04117b34b8a94b9838a93ace08af69c
Danylo2403 <daniilkon24@gmail.com>
2023-09-07 00:55:15 +0300

    number_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items";
 
-const gallery = document.querySelector('.gallery');
+const galleryList = document.querySelector('.gallery');
 
-const markup = createMarkup(galleryItems)
+const galleryMarkup = galleryItems.map(item => `
+<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description} data-source="${item.original}" />
+</a>
+</li>`).join('');
 
-function createMarkup(arr) {
-  return arr.map((card) => {
-    console.log(card);
-    return card; // Повертаємо оригінальний об'єкт card
-  });
-}
+galleryList.innerHTML = galleryMarkup;
+
+galleryList.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.tagName === 'IMG') {
+    const largeImageSrc = e.target.getAttribute('data-source');
+    const lightbox = basicLightbox.create(`
+    <img src="${largeImageSrc}" alt="${e.target.alt}" />
+    `);
+    lightbox.show();
+  }
+});
