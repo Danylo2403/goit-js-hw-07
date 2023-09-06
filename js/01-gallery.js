aca9fab62102c187ccfd1a34fb810af31d5134b5
Danylo2403 <daniilkon24@gmail.com>
2023-09-06 11:54:41 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+//*
+const galleryLi = galleryItems
+    .map(({ preview, original, description }) =>
+        `<li data-preview="${preview}" class="gallery__item js-product-item">
+        <a class="gallery__link" href="${original}">
+        <img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"/>
+        </a>
+        </li>`
+    ).join('');
+
+gallery.innerHTML = galleryLi;
+
+// console.log(galleryItems);
+
+gallery.addEventListener(`click`, evt => {
+    evt.preventDefault();
+    if (evt.target.targetName === `IMG`) {
+        const largeImageSrc = evt.target.getAttribute('data-source');
+        const lightbox = basicLightbox.create(`
+        <img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"/>`)
+        lightbox.show();
+    }
+});
