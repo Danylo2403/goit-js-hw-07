33959809f8251153a69205fa3415105f93771159
Danylo2403 <daniilkon24@gmail.com>
2023-09-07 01:01:08 +0300

    number-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,39 @@
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from './gallery-items.js';
 
-const galleryList = document.querySelector('.gallery');
+// Розмітка карток
+const container = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
 
-const galleryMarkup = galleryItems.map(item => `
-<li class="gallery__item">
-<a class="gallery__link" href="${item.original}">
-<img class="gallery__image" src="${item.preview}" alt="${item.description} data-source="${item.original}" />
-</a>
-</li>`).join('');
+container.insertAdjacentHTML('beforeend', markup);
+container.addEventListener('click', handleProductClick);
 
-galleryList.innerHTML = galleryMarkup;
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => {
+        return `<li data-preview="${preview}" class="gallery__item js-product-item">
+        <a class="gallery__link" href="${original}">
+        <img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"/>
+        </a>
+        </li>`
+    }).join('');
+}
 
-galleryList.addEventListener('click', e => {
-  e.preventDefault();
-  if (e.target.tagName === 'IMG') {
-    const largeImageSrc = e.target.getAttribute('data-source');
-    const lightbox = basicLightbox.create(`
-    <img src="${largeImageSrc}" alt="${e.target.alt}" />
-    `);
-    lightbox.show();
-  }
-});
+// 2) Юзер клікає на картку товару
+
+function handleProductClick(event) {
+    event.preventDefault(); // Забороняємо стандартну дію переходу по посиланню
+    const targetElement = event.target.closest('.js-product-item');
+    
+    if (!targetElement) return; // Перевіряємо, чи клікнули на потрібному елементі
+    
+    const productId = targetElement.dataset.preview;
+    const productInfo = galleryItems.find(product => product.preview === productId);
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <a class="gallery__link" href="${productInfo.original}">
+        <img src="${productInfo.preview}" data-source="${productInfo.original}" alt="${productInfo.description}" class="gallery__image"/>
+        </a>
+    </div>`);
+    instance.show();
+    };
+// console.log(galleryItems);
